48ba61a34d828854744e33788bafb57564b0431b
Edgar chung <edgar.chungmantik@gmail.com>
2023-10-22 08:48:43 +0800

    c-29

--- wef000.js
+++ wef000.js
@@ -1,3 +1,26 @@
+// Exercise 1: Your first program
+
+// var sum = 1 + 1;
+// let sum2 = 1 - 1;
+// const sum3 = (10 * 2 / (2 % 5))
+// console.log(sum, sum2, sum3);
+
+// // par2
+// let PTS = 20; 
+// let REB = 8; 
+// let AST = 5; 
+// let STL = 2; 
+// let BLK = 1; 
+// let missedFg = 3;
+// let missedFt = 1;
+// let T0 = 1;
+// let GP = 2;
+// let efficiency = ((PTS + REB + AST + STL + BLK) - missedFg - missedFt - T0) / GP
+
+// console.log("value of players: " + efficiency);
+
+// ------------------------------------------------------------------------------------------
+
 // Exercise 6: Mark Six Checker
 // Create a function checkMarkSix() that allow you to determine the prizes of Mark Six. For simplicity, we assume only two numbers are required for a bid and only one prize (win or lose).
 // The function usage should be as follow:
@@ -8,6 +31,7 @@
 // The first parameter is the mark six result. The second parameter is the bid.
 
 // The function should return true if the bid is contained in the result or otherwise false.
+
 
 function checkMarkSix(numList, bid) {
   
@@ -29,19 +53,18 @@ const number = bid;
   console.log(checkMarkSix(numList2, bid3)); 
 
   // Bonus 1
-  //Create another function quickPicks() that may quickly pick several bids randomly and check against one single result.
-  //quickPicks([1, 3, 5, 7, 9, 11], 1); // returns: [{"bid": [1, 3], "win": true}]
-  //quickPicks([1, 3, 5, 7, 9, 11], 3); // returns: [{"bid": [2, 4], "win": false}, {"bid": [2, 5], "win": false}, {"bid": [7, 9], "win": true}]
-  //The first parameter is the mark six result. The second parameter is the number of bids.
-  //The function should return an array of the result object with the bid generated and the result from checkMarkSix().
+  // Create another function quickPicks() that may quickly pick several bids randomly and check against one single result.
+  // quickPicks([1, 3, 5, 7, 9, 11], 1); // returns: [{"bid": [1, 3], "win": true}]
+  // quickPicks([1, 3, 5, 7, 9, 11], 3); // returns: [{"bid": [2, 4], "win": false}, {"bid": [2, 5], "win": false}, {"bid": [7, 9], "win": true}]
+  // The first parameter is the mark six result. The second parameter is the number of bids.
+  // The function should return an array of the result object with the bid generated and the result from checkMarkSix().
   //  quickPicks([1, 3, 5, 7, 9, 11], 1)
-  //quickPicks([1, 3, 5, 7, 9, 11], 3) 
+  // quickPicks([1, 3, 5, 7, 9, 11], 3) 
 
 
-//   function quickPicks(numList, bid) {
-//     const numer = bid;
-//     if
+  function quickPicks(numList, bid) {
+    const numer = bid;
+    if
 
-//   }
+  }
 
-  
