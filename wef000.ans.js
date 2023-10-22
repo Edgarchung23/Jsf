7c476a9193e483661209bf4afe1e46d3691fd547
Edgar chung <edgar.chungmantik@gmail.com>
2023-10-23 05:23:52 +0800

    c-29

--- wef000.ans.js
+++ wef000.ans.js
@@ -8,15 +8,55 @@
 // The first parameter is the mark six result. The second parameter is the bid.
 
 // The function should return true if the bid is contained in the result or otherwise false.
-    // console.log(arr1);
-    // console.log(num);
+// Function -> Comparison of two arrays (result, bids)
+// -> for of loop, includes
+// comparison of two arrays
 
+function checkMarkSix(result, bids) {
+  for (let bid of bids) {
+    if (!result.includes(bid)) {
+      //True 才會出現
+      return false;
+    }
+  }
+  return true;
+}
 
+// console.log(checkMarkSix([1, 3, 5, 7, 9, 11], [1, 3]))
+// console.log(checkMarkSix([1, 3, 5, 7, 9, 11], [2, 3]))
+// console.log(checkMarkSix([2, 4, 10, 15, 14, 19], [2, 19]))
 
+// Bonus 1
+// Create another function quickPicks() that may quickly pick several bids randomly and check against one single result.
+// quickPicks([1, 3, 5, 7, 9, 11], 1); // returns: [{"bid": [1, 3], "win": true}]
+// quickPicks([1, 3, 5, 7, 9, 11], 3); // returns: [{"bid": [2, 4], "win": false}, {"bid": [2, 5], "win": false}, {"bid": [7, 9], "win": true}]
+// The first parameter is the mark six result. The second parameter is the number of bids.
+// The function should return an array of the result object with the bid generated and the result from checkMarkSix().
+// quickPicks([1, 3, 5, 7, 9, 11], 1)
+// quickPicks([1, 3, 5, 7, 9, 11], 3)
 
+function quickPicks(result, timeOfbid) {
+  let summary = [];
+  for (i = 0; i < timeOfbid; i++) {
+    // console.log(timeOfbid)
+    let bid = [getRandomInt(), getRandomInt()];
+    summary.push({ bid: bid, "win:": checkMarkSix(result, bid) });
+  }
+  return summary;
+}
+function getRandomInt() {
+  return Math.floor(Math.random() * 20);
+}
+console.log(quickPicks([1, 7, 3, 5, 9, 11], 1));
+console.log(quickPicks([1, 3, 5, 7, 9, 11], 15));
 
+// Bonus2
+// genPicks
+// Continued with the Quick Pick function, do not return the result immediately. Instead, print the result one by one with one second interval.
 
 // function checkArrNum(array, num) {   //************************************
+// console.log(arr1);
+// console.log(num);
 
 //     for (let i = 0; i < array.length; i++) {
 //         // console.log(`Index ${i} value is :${array[i]}`);
