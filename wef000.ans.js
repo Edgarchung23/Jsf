71313ec674673c8dc39e9ca2894cbbde77de0694
Edgar chung <edgar.chungmantik@gmail.com>
2023-10-22 11:04:56 +0800

    c-29

---
+++ wef000.ans.js
@@ -0,0 +1,10 @@
+// Exercise 6: Mark Six Checker
+// Create a function checkMarkSix() that allow you to determine the prizes of Mark Six. For simplicity, we assume only two numbers are required for a bid and only one prize (win or lose).
+// The function usage should be as follow:
+
+// checkMarkSix([1, 3, 5, 7, 9, 11], [1, 3]); // returns: true
+// checkMarkSix([1, 3, 5, 7, 9, 11], [2, 3]); // returns: false
+// checkMarkSix([2, 4, 10, 15, 14, 19], [2, 19]); // returns: true
+// The first parameter is the mark six result. The second parameter is the bid.
+
+// The function should return true if the bid is contained in the result or otherwise false.
